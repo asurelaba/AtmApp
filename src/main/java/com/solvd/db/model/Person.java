7800f7ad58e3e89474b6fc6f66dfa6ac95422715
asurelaba <asuresh.laba@solvd.com>
2023-07-02 02:36:40 -0400

    add package for db

---
+++ src/main/java/com/solvd/db/model/Person.java
@@ -0,0 +1,32 @@
+package com.solvd.db.model;
+
+public class Person {
+
+    private int personId;
+    private String firstName;
+    private String lastName;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
