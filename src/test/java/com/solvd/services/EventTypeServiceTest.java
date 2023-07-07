6caa72ee11af91bec02e2494762b200e9bbf13b0
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-07-06 22:30:24 -0700

    added test cases for service layer eventType, person, userRole, and user

---
+++ src/test/java/com/solvd/services/EventTypeServiceTest.java
@@ -0,0 +1,46 @@
+package com.solvd.services;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import com.solvd.db.model.EventType;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class EventTypeServiceTest {
+
+    EventTypeService et;
+
+    @BeforeMethod
+    public void setUp() {
+        et = new EventTypeService();
+    }
+
+    @Test
+    public void testGetEventTypeByTypeName() {
+        EventType withdrawalType = et.getEventTypeByTypeName("Withdrawal");
+        assertEquals(withdrawalType.getName(), "Withdrawal");
+    }
+
+    @Test
+    public void testGetAll() {
+        List<EventType> allEventTypes = et.getAll();
+        Set<EventType> setEventTypes = new HashSet<>(allEventTypes);
+        String[] eventTypes = {"log In", "Log Out", "Lock Card", "Transaction Query",
+            "Check Balance", "Print Receipt", "Change Pin", "Unlock Card Request", "Withdrawal",
+            "Deposit"
+        };
+        for (String eventType : eventTypes) {
+            assertTrue(setEventTypes.stream()
+                .map(EventType::getName)
+                .collect(Collectors.toSet())
+                .contains(eventType));
+        }
+    }
+
+
+}
---
+++ src/test/java/com/solvd/services/UserRoleServiceTest.java
@@ -0,0 +1,27 @@
+package com.solvd.services;
+
+import static org.testng.Assert.*;
+
+import com.solvd.db.model.UserRole;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class UserRoleServiceTest {
+
+    private UserRoleService ur;
+
+    @BeforeMethod
+    public void setUp() {
+        ur = new UserRoleService();
+    }
+
+    @Test
+    public void testGetUserRoleByRoleName() {
+        UserRole clientRole = ur.getUserRoleByRoleName("Client");
+        UserRole adminRole = ur.getUserRoleByRoleName("Administrator");
+        assertNotEquals(clientRole, adminRole);
+        assertEquals(clientRole.getName(), "Client");
+        assertEquals(adminRole.getName(), "Administrator");
+    }
+
+}
