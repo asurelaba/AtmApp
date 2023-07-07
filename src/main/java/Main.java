42fa640f8888ac34f2eb47e0572df203839e28f2
andresmadeddie <abinary1010@gmail.com>
2023-07-07 09:10:56 -0400

    create a logo class/method and implemented in the main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import com.solvd.Logo;
 import com.solvd.controllers.atm.AtmController;
 
 public class Main {
     public static void main(String[] args) {
+        Logo.logo();
         AtmController atmController = new AtmController();
         atmController.run();
     }
---
+++ src/main/java/com/solvd/Logo.java
@@ -0,0 +1,15 @@
+package com.solvd;
+
+public class Logo {
+    public static void logo(){
+        System.out.println("\n\n");
+        System.out.println("     AAA      TTTTTTTTTT  MMMM     MMMM    ");
+        System.out.println("    AAAAA     TTTTTTTTTT  MMMM     MMMM     /\\_/\\");
+        System.out.println("   A     A        TT      MM MM   MM MM    ( o.o )");
+        System.out.println("  AAAAAAAAA       TT      MM  MM MM  MM     > ^ <");
+        System.out.println(" A         A      TT      MM   MMM   MM    /  _  \\");
+        System.out.println("A           A     TT      MM         MM   /_/   \\_\\");
+        System.out.println("\n\n");
+    }
+
+}
