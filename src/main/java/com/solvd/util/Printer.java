792a974ee2d75ff3b8c9347ebfc8ff43ff1abda2
andres <agarcianavarro.laba@solvd.com>
2023-07-09 14:50:04 -0400

    fix Printer to print lines. tested with local printer

--- src/main/java/com/solvd/util/Printer.java
+++ src/main/java/com/solvd/util/Printer.java
@@ -9,27 +9,24 @@ public class Printer {
 
     public static void print(String content) throws PrinterException {
         PrinterJob printerJob = PrinterJob.getPrinterJob();
-        Printable printable = new Printable() {
-            @Override
-            public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
-                if (pageIndex > 0) {
-                    return Printable.NO_SUCH_PAGE;
-                }
-
-                Graphics2D g2d = (Graphics2D) graphics;
-                g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-
-                String[] lines = content.split("\n");
-                int y = 100;
-                Font font = new Font("Arial", Font.PLAIN, 12);
-                g2d.setFont(font);
-                for (String line : lines) {
-                    g2d.drawString(line, 100, y);
-                    y += 15; // Adjust the line spacing as needed
-                }
-
-                return Printable.PAGE_EXISTS;
+        Printable printable = (graphics, pageFormat, pageIndex) -> {
+            if (pageIndex > 0) {
+                return Printable.NO_SUCH_PAGE;
             }
+
+            Graphics2D g2d = (Graphics2D) graphics;
+            g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+
+            String[] lines = content.split("\n");
+            int y = 100;
+            Font font = new Font("Arial", Font.PLAIN, 12);
+            g2d.setFont(font);
+            for (String line : lines) {
+                g2d.drawString(line, 100, y);
+                y += 15; // Adjust the line spacing as needed
+            }
+
+            return Printable.PAGE_EXISTS;
         };
 
         printerJob.setPrintable(printable);
