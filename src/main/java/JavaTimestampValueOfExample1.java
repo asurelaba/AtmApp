a5b3ff16ed4f092bcad890dd320bb140544b10c0
andresmadeddie <abinary1010@gmail.com>
2023-07-11 13:11:41 -0400

    Modify mappers with the new renamed methods of DateRange in transactions

--- src/main/java/JavaTimestampValueOfExample1.java
+++ src/main/java/JavaTimestampValueOfExample1.java
@@ -1,21 +1,27 @@
+import com.solvd.services.TransactionService;
+
 import java.sql.Timestamp;
 import java.util.Scanner;
 
 public class JavaTimestampValueOfExample1 {
     public static void main(String[] args) {
-        String str ="2018-09-01 09:01:15";
-        System.out.println("String : "+str);
-        // returns a Timestamp value corresponding to the given string
-        Timestamp timestamp= Timestamp.valueOf(str);
-        System.out.println("value of Timestamp : "+timestamp);
+        String one ="2023-07-01 00:00:00";
+        String two ="2023-07-12 00:00:00";
 
-        Scanner scanner = new Scanner(System.in);
+        // returns a Timestamp value corresponding to the given string
+        Timestamp from = Timestamp.valueOf(one);
+        Timestamp to = Timestamp.valueOf(two);
+
+
+/*        Scanner scanner = new Scanner(System.in);
         System.out.println("now");
         String str2 = scanner.nextLine();
         System.out.println("one " + str);
         System.out.println("two " + str2);
         Timestamp timestamp2= Timestamp.valueOf(str2);
-        System.out.println("TWO: "+timestamp2);
+        System.out.println("TWO: "+timestamp2);*/
+
+        System.out.println(new TransactionService().getTransactionsByDateRange(from,to));
 
     }
 }
