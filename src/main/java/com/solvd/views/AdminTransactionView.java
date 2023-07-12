d4f2369bcf0fd9535b3bff5506f11632a3f15b67
andresmadeddie <abinary1010@gmail.com>
2023-07-12 17:37:02 -0400

    Add a \n to create a new line in receipt creator to print the date add displaybody, exitrun and displaytile handle exceptions in status method

--- src/main/java/com/solvd/views/AdminTransactionView.java
+++ src/main/java/com/solvd/views/AdminTransactionView.java
@@ -3,6 +3,7 @@ package com.solvd.views;
 import com.solvd.views.atm.AbstractAtmView;
 
 import java.sql.Timestamp;
+import java.util.InputMismatchException;
 
 public class AdminTransactionView extends AbstractAtmView {
 
@@ -12,15 +13,15 @@ public class AdminTransactionView extends AbstractAtmView {
     }
 
     public void displayAdminTransactionViewMenu() {
-        display(System.lineSeparator());
-        display(featureTitle());
-        display("1. Query by Status");
-        display("2. Query by Event Id");
-        display("3. Query by Card Number");
-        display("4. Query by Date");
-        display("5. Query by User Id");
-        display("6. Query by User Id and Date");
-        display("7. return to main menu");
+        displayTitle(System.lineSeparator());
+        displayTitle(featureTitle());
+        displayBody("1. Query by Status");
+        displayBody("2. Query by Event Id");
+        displayBody("3. Query by Card Number");
+        displayBody("4. Query by Date");
+        displayBody("5. Query by User Id");
+        displayBody("6. Query by User Id and Date");
+        displayBody("7. return to main menu");
     }
 
     public int getUserInputInt() {
@@ -33,8 +34,8 @@ public class AdminTransactionView extends AbstractAtmView {
             try {
                 return Integer.parseInt(input);
             } catch (NumberFormatException e) {
-                display("Invalid input. Please enter a valid integer.");
-                display("Press 'q' to quit or enter a valid integer to try again:");
+                displayBody("Invalid input. Please enter a valid integer.");
+                displayBody("Press 'q' to quit or enter a valid integer to try again:");
             }
         }
     }
@@ -49,14 +50,27 @@ public class AdminTransactionView extends AbstractAtmView {
             try {
                 return Long.parseLong(input);
             } catch (NumberFormatException e) {
-                display("Invalid input. Please enter a valid long integer.");
-                display("Press 'q' to quit or enter a valid long integer to try again:");
+                displayBody("Invalid input. Please enter a valid long integer.");
+                displayBody("Press 'q' to quit or enter a valid long integer to try again:");
             }
         }
     }
 
     public String getUserInputString() {
-        return s.next();
+        while (true) {
+            String input = s.next();
+            if (input.equalsIgnoreCase("q")) {
+                return null;
+            }
+
+            try {
+                return s.next();
+            } catch (InputMismatchException e) {
+                s.next();
+                displayBody("Invalid input. Valid Option: approved");
+                displayBody("Press 'q' to quit or enter a valid date to try again:");
+            }
+        }
     }
 
     public Timestamp getUserInputDate() {
@@ -70,8 +84,8 @@ public class AdminTransactionView extends AbstractAtmView {
                 return Timestamp.valueOf(input + " 00:00:00");
             } catch (IllegalArgumentException e) {
                 s.nextLine();
-                display("Invalid date format. Please enter a valid date in the format 'YYYY-MM-DD'.");
-                display("Press 'q' to quit or enter a valid date to try again:");
+                displayBody("Invalid date format. Please enter a valid date in the format 'YYYY-MM-DD'.");
+                displayBody("Press 'q' to quit or enter a valid date to try again:");
             }
         }
     }
