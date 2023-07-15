b70590ac4d25a73051447350b1402095c1d2d5b6
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-07-14 22:45:40 -0700

    added info in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
+import static com.solvd.util.AppConfig.DB;
+import static com.solvd.util.AppConfig.ENVIRONMENT;
+
 import com.solvd.controllers.atm.AtmController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        if (ENVIRONMENT.equals("DEV")) {
+            Logger LOG = LogManager.getLogger(Main.class);
+            LOG.warn("ENVIRONMENT: DEV");
+            LOG.warn("DB: " + DB);
+            LOG.warn("Configure DB (SQLITE or MYSQL) in resources/appconfig.properties");
+        }
+
         AtmController atmController = new AtmController();
         atmController.run();
     }
